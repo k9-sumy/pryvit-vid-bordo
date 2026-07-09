7609917bfe0c208029dec54ae67209713420fe6d
k9-sumy <6p88ycjysk@privaterelay.appleid.com>
2026-07-09 23:39:46 +0300

    Add createPaw function to generate paw elements

--- script.js
+++ script.js
@@ -29,3 +29,24 @@ let random=Math.floor(Math.random()*wishes.length);
 document.getElementById("wish").innerHTML=wishes[random];
 
 }
+function createPaw(){
+
+const paw=document.createElement("div");
+
+paw.innerHTML="🐾";
+
+paw.className="paw";
+
+paw.style.left=Math.random()*100+"vw";
+
+paw.style.animationDuration=3+Math.random()*3+"s";
+
+document.getElementById("paw-container").appendChild(paw);
+
+setTimeout(()=>{
+
+paw.remove();
+
+},6000);
+
+}
